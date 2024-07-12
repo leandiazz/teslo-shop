4d19f0e5414abc9881965c944c92e90a4b3da047
Leandro Diaz <Leandrodiaz042@gmail.com>
2024-07-12 15:57:45 -0300

    feature: add top menu component

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,7 +1,14 @@
+import { TopMenu } from "@/components";
+
 export default function ShopLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return <div>{children}</div>;
+    return (
+        <div className="min-h-screen">
+            <TopMenu />
+            {children}
+        </div>
+    );
 }
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,7 +2,7 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
     return (
-        <main className="">
+        <main>
             <h1>Hola Mundo</h1>
             <h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
         </main>
