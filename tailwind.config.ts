e065de104d7c36854c4f2cf1a53b6fa5db5fe21e
Leandro Diaz <Leandrodiaz042@gmail.com>
2024-07-17 18:20:07 -0300

    update: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,9 @@ const config: Config = {
             backgroundImage: {
                 "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
                 "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
-            }
+            },
         }
     },
-    plugins: []
+    plugins: [],
 };
 export default config;
