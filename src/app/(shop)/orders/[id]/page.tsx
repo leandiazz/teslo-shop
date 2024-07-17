dc68fa9dd3d0b804119466c1ccd1f5f0196344be
Leandro Diaz <Leandrodiaz042@gmail.com>
2024-07-17 18:19:17 -0300

    feat: add order detail page

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,7 +1,111 @@
-export default function OrderPage() {
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import clsx from "clsx";
+import Image from "next/image";
+import { IoCardOutline } from "react-icons/io5";
+
+const productsInCart = [initialData.products[0], initialData.products[1], initialData.products[2]];
+
+interface Props {
+    params: {
+        id: string;
+    };
+}
+
+export default function OrdersPage({ params }: Props) {
+    const { id } = params;
+
+    // TODO: Verificar que le pertenezca a la persona
+    //  redirect
+
     return (
-        <div>
-            <h1>Single Order Page</h1>
+        <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+            <div className="flex flex-col w-[1000px]">
+                <Title title={`Orden #${id}`} />
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+                    {/* Carrito */}
+                    <div className="flex flex-col mt-5">
+                        <div
+                            className={clsx(
+                                "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
+                                {
+                                    "bg-red-500": true,
+                                    "bg-green-500": true
+                                }
+                            )}
+                        >
+                            <IoCardOutline size={30} />
+                            {/* <span className="mx-2">Pendiente de pago</span> */}
+                            <span className="mx-2">Pagado</span>
+                        </div>
+                        {/* Items */}
+                        {productsInCart.map(product => (
+                            <div key={product.slug} className="flex mb-5">
+                                <Image
+                                    src={`/products/${product.images[0]}`}
+                                    width={100}
+                                    height={100}
+                                    alt={product.title}
+                                    style={{
+                                        width: "100px",
+                                        height: "100px"
+                                    }}
+                                    className="mr-5 rounded object-contain"
+                                />
+                                <div>
+                                    <p>{product.title}</p>
+                                    <p>${product.price} x 3</p>
+                                    <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                                    <button type="button" className="underline mt-3">
+                                        Remover
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    {/* Checkout */}
+                    <div className="bg-white rounded-xl shadow-xl p-7">
+                        <h2 className="text-xl mb-2 font-bold">Dirección de entrega</h2>
+                        <div className="mb-10">
+                            <p className="text-xl">Leandro Diaz</p>
+                            <p>Ciudad Buenos Aires</p>
+                            <p>Av. Frontend Developer</p>
+                            <p>CP 123123</p>
+                            <p>123.123.123</p>
+                        </div>
+                        {/* Divider */}
+                        <div className="w-full h-[0.5px] rounded bg-gray-200 mb-10" />
+                        <h2 className="text-xl mb-2 font-bold">Resumen de orden</h2>
+                        <div className="grid grid-cols-2">
+                            <span>No. Productos</span>
+                            <span className="text-right">3 articulos</span>
+
+                            <span>Subtotal</span>
+                            <span className="text-right">$ 100</span>
+                            <span>Impuestos (15%)</span>
+                            <span className="text-right">$ 100</span>
+                            <span className="mt-5 text-2xl">Total</span>
+                            <span className="mt-5 text-2xl text-right">$ 100</span>
+                        </div>
+
+                        <div className="mt-5 mb-2 w-full">
+                            <div
+                                className={clsx(
+                                    "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
+                                    {
+                                        "bg-red-500": true,
+                                        "bg-green-500": true
+                                    }
+                                )}
+                            >
+                                <IoCardOutline size={30} />
+                                {/* <span className="mx-2">Pendiente de pago</span> */}
+                                <span className="mx-2">Pagado</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
