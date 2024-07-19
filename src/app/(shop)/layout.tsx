f618f0c67acfdef14d1829a880845f926e5afac5
Leandro Diaz <Leandrodiaz042@gmail.com>
2024-07-19 18:25:59 -0300

    feat: add footer

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,11 +1,16 @@
-import { Sidebar, TopMenu } from "@/components";
+import { Footer, Sidebar, TopMenu } from "@/components";
 
-export default function ShopLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="min-h-screen">
-            <TopMenu />
-            <Sidebar />
-            <div className="px-0 sm:px-10">{children}</div>
-        </div>
-    );
+export default function ShopLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen">
+      <TopMenu />
+      <Sidebar />
+      <div className="px-0 sm:px-10">{children}</div>
+      <Footer />
+    </div>
+  );
 }
